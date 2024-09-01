c8d5933ce2d4c7da6e3358234c428e4d4789cbea
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-01 19:50:06 +0330

    feat: organziation profile page

--- src/pages/profile/org/index.tsx
+++ src/pages/profile/org/index.tsx
@@ -1,3 +1,17 @@
+import ManageOrg from 'src/modules/Profile/containers/ManageOrg';
+
+import css from './index.module.scss';
+
 export const OrgProfile = () => {
-  return <div>Organization profile</div>;
+  return (
+    <div className={css['container']}>
+      <div className={css['top']}>
+        <div className={css['top__header']}>
+          <h1 className={css['top__title']}>Organization profile</h1>
+          <h2 className={css['top__subtitle']}>Update your logo and organization details here.</h2>
+        </div>
+      </div>
+      <ManageOrg />
+    </div>
+  );
 };
