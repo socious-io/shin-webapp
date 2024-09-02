54ec9935701b2dc2905df7f0dec65bcc0e8aa2a1
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-03 00:26:52 +0330

    feat: delete account section in settings page

---
+++ src/modules/Settings/containers/DeleteAccount/index.tsx
@@ -0,0 +1,24 @@
+import { Divider } from '@mui/material';
+import Button from 'src/modules/General/components/Button';
+
+import css from './index.module.scss';
+import { useDeleteAccount } from './useDeleteAccount';
+
+const DeleteAccount = () => {
+  const {
+    data: { translate },
+    operations: onDeleteAccount,
+  } = useDeleteAccount();
+
+  return (
+    <div className={css['container']}>
+      <span className={css['title']}>{translate('settings-remove.title')}</span>
+      <Divider className="mx-[-1.5rem]" />
+      <Button color="error" type="button" customStyle="self-end" onClick={onDeleteAccount}>
+        {translate('settings-remove.title')}
+      </Button>
+    </div>
+  );
+};
+
+export default DeleteAccount;
