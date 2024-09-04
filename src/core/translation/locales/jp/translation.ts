0a0e3e60b4a119c6287ccc1e11d73a1688f111e5
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-04 21:12:49 +0330

    feat: add translations for pagination general component

--- src/core/translation/locales/jp/translation.ts
+++ src/core/translation/locales/jp/translation.ts
@@ -1,5 +1,6 @@
 import fileUploader from './fileUploader.json';
 import forgetPassword from './forgetPassword.json';
+import pagination from './general/pagination.json';
 import orgProfile from './organizations/organizations.json';
 import schema from './schemas/schema.json';
 import schemaCreate from './schemas/schemaCreate.json';
@@ -16,6 +17,7 @@ import proofRequest from './verifications/proofRequest.json';
 export function generateTranslationFile() {
   return Object.assign(
     {},
+    pagination,
     signIn,
     verification,
     detail,
