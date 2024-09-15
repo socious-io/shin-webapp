21dd57200e45cfddb4895d746aa818501d982501
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-15 23:33:40 +0330

    refactor: made card radio buttton compoent same as design

--- src/modules/General/components/CardRadioButton/index.tsx
+++ src/modules/General/components/CardRadioButton/index.tsx
@@ -1,55 +1,40 @@
-import { RadioGroup } from '@mui/material';
-import variables from 'src/styles/constants/_exports.module.scss';
+import { Radio, RadioGroup } from '@mui/material';
 
 import css from './index.module.scss';
 import { CardRadioButtonProps } from './index.types';
-import Checkbox from '../Checkbox';
-import Icon from '../Icon';
 
-const CardRadioButton: React.FC<CardRadioButtonProps> = props => {
-  const { items, selectedValue, setSelectedValue } = props;
+const CardRadioButton: React.FC<CardRadioButtonProps> = ({
+  items,
+  selectedValue,
+  setSelectedValue,
+  customStyle = '',
+  containerClassName = '',
+}) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(event.target.value);
   };
 
   return (
-    <RadioGroup sx={{ gap: '12px' }}>
+    <RadioGroup className={`gap-3 ${customStyle}`}>
       {items.map(item => (
         <div
           key={item.title}
-          className={`${css.container} ${selectedValue === item.value ? css.selectedContainer : css.normalContainer} `}
+          className={`${css['container']} ${selectedValue === item.value && css['container--selected']} ${containerClassName}`}
           onClick={() => setSelectedValue(item.value)}
         >
-          {item.img ? (
-            item.img
-          ) : item.icon ? (
-            <div className={css.iconDiv}>
-              <Icon
-                name={item.icon.name}
-                fontSize={item.icon.fontSize}
-                color={selectedValue === item.value ? variables.color_primary_600 : variables.color_primary_600}
-              />
-            </div>
-          ) : (
-            ''
-          )}
-
-          <div className={css.content}>
-            <div className={`${css.title} ${selectedValue === item.value ? css.selectedTitle : ''}`}>{item.title}</div>
-            <div className={`${css.desc} ${selectedValue === item.value ? css.selectedDesc : ''}`}>
-              {item.description}
-            </div>
+          {item.icon}
+          <div className={css['content']}>
+            <div className={css['content__title']}>{item.title}</div>
+            <div className={css['content__desc']}>{item.description}</div>
           </div>
-          <div className={css.rbContainer}>
-            <Checkbox
-              id={item.title}
-              type="checkCircle"
-              size="small"
-              onChange={handleChange}
-              value={item.value}
-              checked={selectedValue === item.value}
-            />
-          </div>
+          <Radio
+            id={item.title}
+            size={item?.radioSize || 'medium'}
+            onChange={handleChange}
+            value={item.value}
+            checked={selectedValue === item.value}
+            className="p-0"
+          />
         </div>
       ))}
     </RadioGroup>
