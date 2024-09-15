9366eefd5f1b129b85753e85b480f54fec381ced
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-15 23:47:42 +0330

    feat: step-1 select schema in creating credential

---
+++ src/modules/Credential/containers/SelectSchema/index.tsx
@@ -0,0 +1,38 @@
+import { Divider } from '@mui/material';
+import Button from 'src/modules/General/components/Button';
+import CardRadioButton from 'src/modules/General/components/CardRadioButton';
+
+import css from './index.module.scss';
+import { SelectSchemaProps } from './index.types';
+import { useSelectSchema } from './useSelectSchema';
+
+const SelectSchema: React.FC<SelectSchemaProps> = ({ schemaRadioItems, selectedSchema, onSelectSchema }) => {
+  const {
+    data: { translate },
+    operations: { onCreateSchema },
+  } = useSelectSchema();
+
+  return (
+    <div className={css['container']}>
+      <div className={css['top']}>
+        <div className={css['top__header']}>
+          <h1 className={css['top__title']}>{translate('credential-step1.header')}</h1>
+          <h2 className={css['top__subtitle']}>{translate('credential-step1.subheader')}</h2>
+        </div>
+        <Button color="primary" onClick={onCreateSchema}>
+          {translate('credential-step1.button')}
+        </Button>
+      </div>
+      <Divider />
+      <CardRadioButton
+        items={schemaRadioItems}
+        selectedValue={selectedSchema}
+        setSelectedValue={onSelectSchema}
+        customStyle={css['select']}
+        containerClassName={css['select__box']}
+      />
+    </div>
+  );
+};
+
+export default SelectSchema;
