147399eb3ec5bd77e7cdafc042217e3a3b9472d7
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-15 23:54:45 +0330

    refactor: some changes in issued list of credentials

--- src/modules/Credential/containers/IssuedList/useIssuedList.tsx
+++ src/modules/Credential/containers/IssuedList/useIssuedList.tsx
@@ -1,26 +1,42 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLoaderData } from 'react-router-dom';
-import { CredentialsRes, CredentialStatus, deleteCredentialAdaptor, revokeCredentialAdaptor } from 'src/core/adaptors';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { config } from 'src/config';
+import {
+  CredentialsRes,
+  CredentialStatus,
+  deleteCredentialAdaptor,
+  getCredentialsAdaptor,
+  revokeCredentialAdaptor,
+} from 'src/core/adaptors';
 import { StatusProps } from 'src/modules/General/components/Status/index.types';
 
 export const useIssuedList = () => {
   const { t: translate } = useTranslation();
+  const navigate = useNavigate();
   const { credentialList } = (useLoaderData() as { credentialList: CredentialsRes }) || {};
-  const { items: list } = credentialList || [];
-  const [currentList, setCurrentList] = useState(list);
+  const [currentCredentialList, setCurrentCredentialList] = useState(credentialList);
+  const currentList = currentCredentialList?.items || [];
   const [selectedCredential, setSelectedCredential] = useState('');
-  const [openModal, setOpenModal] = useState<{ name: 'revoke' | 'delete' | ''; open: boolean }>({
+  const [openModal, setOpenModal] = useState<{ name: 'revoke' | 'delete' | 'copy' | ''; open: boolean }>({
     name: '',
     open: false,
   });
+  const [url, setUrl] = useState('');
   const [page, setPage] = useState(1);
-  const totalPage = 1;
+  const totalPage = Math.ceil((currentCredentialList?.total || 1) / (currentCredentialList?.limit || 10));
 
-  const status: Record<Exclude<CredentialStatus, 'ISSUED'>, StatusProps> = {
+  const status: Record<CredentialStatus, StatusProps> = {
+    ISSUED: { label: translate('credential-status.issued'), theme: 'warning' },
     PENDING: { label: translate('credential-status.pending'), theme: 'warning' },
     ACTIVE: { label: translate('credential-status.active'), theme: 'success' },
     REVOKED: { label: translate('credential-status.revoked'), theme: 'error' },
+  };
+
+  const onChangePage = async (newPage: number) => {
+    setPage(newPage);
+    const { data } = await getCredentialsAdaptor(newPage);
+    data && setCurrentCredentialList(data);
   };
 
   const onSelectCredential = (id: string) => {
@@ -37,43 +53,56 @@ export const useIssuedList = () => {
   const onRevokeClick = () => setOpenModal({ name: 'revoke', open: true });
 
   const onRevokeCredential = async () => {
-    const { error } = await revokeCredentialAdaptor(selectedCredential);
-    if (error) {
-      console.log(error);
-      return;
+    if (selectedCredential) {
+      const { error } = await revokeCredentialAdaptor(selectedCredential);
+      if (error) return;
+      const revokedList = currentList.map(list =>
+        list.id === selectedCredential ? { ...list, status: 'REVOKED' as CredentialStatus } : list,
+      );
+      setCurrentCredentialList({ ...currentCredentialList, items: revokedList });
+      setSelectedCredential('');
+      handleCloseModal();
     }
-    const revokedCredential = currentList.map(list =>
-      list.id === selectedCredential ? { ...list, status: 'REVOKED' as CredentialStatus } : list,
-    );
-    setCurrentList(revokedCredential);
-    setSelectedCredential('');
-    handleCloseModal();
   };
 
   const onDeleteClick = () => setOpenModal({ name: 'delete', open: true });
 
   const onDeleteCredential = async () => {
-    const { error } = await deleteCredentialAdaptor(selectedCredential);
-    if (error) {
-      console.log(error);
-      return;
+    if (selectedCredential) {
+      const { error } = await deleteCredentialAdaptor(selectedCredential);
+      if (error) return;
+      const filteredList = currentList.filter(list => list.id !== selectedCredential);
+      onChangePage(filteredList.length === 0 && page > 1 ? page - 1 : page);
+      setSelectedCredential('');
+      handleCloseModal();
     }
-    const deletedCredential = currentList.filter(list => list.id !== selectedCredential);
-    setCurrentList(deletedCredential);
-    setSelectedCredential('');
-    handleCloseModal();
   };
 
+  const onCopyClick = (credentialId: string) => {
+    const copyUrl = `${config.appBaseURL}/proof-request/credential/${credentialId}`;
+    setUrl(copyUrl);
+    setOpenModal({ name: 'copy', open: true });
+  };
+
+  const onCopyCredential = () => {
+    navigator.clipboard.writeText(url);
+  };
+
+  const onCreateCredential = () => navigate('create');
+
   return {
-    data: { translate, currentList, page, totalPage, selectedCredential, status, openModal },
+    data: { translate, currentList, page, totalPage, selectedCredential, status, openModal, url },
     operations: {
-      setPage,
+      onChangePage,
       onSelectCredential,
       handleCloseModal,
       onRevokeClick,
       onRevokeCredential,
       onDeleteClick,
       onDeleteCredential,
+      onCopyClick,
+      onCopyCredential,
+      onCreateCredential,
     },
   };
 };
