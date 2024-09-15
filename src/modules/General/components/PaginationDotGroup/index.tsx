3a0459e8e6abe40dd8305901d3aae368accda3c6
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-15 23:31:58 +0330

    refactor: made generalize pagination dot group component

--- src/modules/General/components/PaginationDotGroup/index.tsx
+++ src/modules/General/components/PaginationDotGroup/index.tsx
@@ -4,14 +4,16 @@ import css from './index.module.scss';
 import { PaginationDotGroupProps } from './index.types';
 
 const PaginationDotGroup: React.FC<PaginationDotGroupProps> = props => {
-  const { shape = 'oval', count, transparent = true, active = 0, size } = props;
+  const { shape = 'oval', count, transparent = true, active = 0, size, titles = [], customStyle = '' } = props;
   return (
     <div className={`${css['container']} ${transparent && 'bg-transparent'}`}>
       {[...Array(count)].map((e, n) => (
-        <div
-          key={n}
-          className={`${css['indicator']} ${css[`indicator--${size}`]} ${css[`indicator--${shape}`]} ${n === active && css['indicator--active']}`}
-        />
+        <div key={n} className={`${css['step']} ${css[`step--${shape}`]} ${customStyle}`}>
+          <div
+            className={`${css['indicator']} ${css[`indicator--${size}`]} ${n === active && css['indicator--active']}`}
+          />
+          {titles[n]}
+        </div>
       ))}
     </div>
   );
