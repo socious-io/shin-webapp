07ffe3a8335d2e7ce2ab9a2ec408f75f4de3412c
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-15 23:49:00 +0330

    feat: step-2 fill claims in creating credential

---
+++ src/modules/Credential/containers/SchemaAttributesForm/useSchemaAttributesForm.tsx
@@ -0,0 +1,62 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useImperativeHandle, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Attribute } from 'src/core/adaptors';
+import { CredentialClaims } from 'src/core/api';
+import { emailPattern, urlPattern } from 'src/core/helpers/regexs';
+import * as yup from 'yup';
+
+const createValidationSchema = (schemaAttributes: Attribute[]) => {
+  const shape: { [key: string]: any } = {};
+  const validations = {
+    TEXT: yup.string().required('This field is required'),
+    NUMBER: yup.number().typeError('This field must be a number').required('This field is required'),
+    BOOLEAN: yup.boolean().default(false),
+    URL: yup.string().matches(urlPattern, 'This field must be a valid URL').required('This field is required'),
+    EMAIL: yup
+      .string()
+      .email()
+      .matches(emailPattern, 'This field must be a valid Email')
+      .required('This field is required'),
+    DATETIME: yup.date().typeError('This field must be a valid date').required('This field is required'),
+  };
+
+  schemaAttributes.forEach(attribute => {
+    shape[attribute.name] = validations[attribute.option.value];
+  });
+  return yup.object().shape(shape).required();
+};
+
+export const useSchemaAttributesForm = (
+  schemaAttributes: Attribute[],
+  onSubmitClaims: (claims: CredentialClaims[]) => void,
+  ref,
+) => {
+  const validationSchema = createValidationSchema(schemaAttributes);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'all',
+    resolver: yupResolver(validationSchema),
+  });
+  const formRef = useRef<HTMLFormElement>(null);
+  useImperativeHandle(ref, () => ({
+    submitForm: () => handleSubmit(onSubmit)(),
+  }));
+
+  const onSubmit = formData => {
+    const claims: CredentialClaims[] =
+      Object.keys(formData).map(key => ({
+        name: key,
+        value: formData[key],
+      })) || [];
+    onSubmitClaims(claims);
+  };
+
+  return {
+    data: { register, control, errors, formRef },
+  };
+};
