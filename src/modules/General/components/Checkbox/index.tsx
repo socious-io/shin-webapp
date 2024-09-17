0d8c26a70ae982da018c3f151fcfff9fd577112d
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-17 20:05:02 +0330

    refactor: controlled checkbox and validations

--- src/modules/General/components/Checkbox/index.tsx
+++ src/modules/General/components/Checkbox/index.tsx
@@ -1,72 +1,98 @@
-import { Checkbox as MUICheckbox } from '@mui/material';
+import { FormControlLabel, Checkbox as MUICheckbox } from '@mui/material';
 import React from 'react';
+import { Controller } from 'react-hook-form';
 import variables from 'src/styles/constants/_exports.module.scss';
 
 import css from './index.module.scss';
-import { CheckboxProps, IconProps } from './index.types';
+import { CheckboxProps } from './index.types';
 import Icon from '../Icon';
-
-const SquareIcon: React.FC<IconProps> = ({ checked, size }) => {
-  return (
-    <div
-      className={`${size === 'small' ? 'w-4 h-4 ' : 'w-5 h-5 '} ${css.icon} ${!checked && css.iconNotChecked} ${
-        checked && css.iconChecked
-      }`}
-    >
-      {checked && <Icon name="check" color={variables.color_primary_600} fontSize={size === 'small' ? 12 : 14} />}
-    </div>
-  );
-};
-
-const CircleIcon: React.FC<IconProps> = ({ checked, size }) => {
-  return (
-    <div
-      className={` rounded-lg flex items-center justify-center ${size === 'small' ? 'w-4 h-4 ' : 'w-5 h-5 '} ${
-        !checked && css.circleIconNotChecked
-      } ${checked && css.circleIconChecked}`}
-    >
-      {checked && <Icon name="check" color="white" fontSize={size === 'small' ? 10 : 14} />}
-    </div>
-  );
-};
 
 const Checkbox: React.FC<CheckboxProps> = ({
   name,
   id,
   label,
-  required,
-  register,
   errors,
-  isValid,
-  type = 'checkBox',
+  type = 'square',
   size = 'small',
+  control,
   ...props
 }) => {
+  const checkboxSize = size === 'small' ? 'w-4 h-4 ' : 'w-5 h-5';
+  const fontSize = size === 'small' ? 12 : 14;
+
   return (
-    <div className={css.container}>
-      <div className={css.checkboxContainer}>
-        <MUICheckbox
-          id={id}
-          disableRipple
-          color="default"
-          checkedIcon={type === 'checkBox' ? <SquareIcon checked size={size} /> : <CircleIcon checked size={size} />}
-          icon={
-            type === 'checkBox' ? (
-              <SquareIcon checked={false} size={size} />
-            ) : (
-              <CircleIcon checked={false} size={size} />
-            )
-          }
-          sx={{ padding: '2px 0 0 0' }}
-          {...(register ? register(name) : {})}
-          {...props}
-        />
-        {label && (
-          <label htmlFor={id} className={css.label} aria-describedby={id}>
-            {label}
-          </label>
+    <div className={css['container']}>
+      <div className={`${css['checkbox']} ${errors ? css['checkbox--error'] : css['checkbox--normal']}`}>
+        {control ? (
+          <FormControlLabel
+            control={
+              <Controller
+                name={name || ''}
+                control={control}
+                rules={{ required: props.required }}
+                render={({ field: { onChange, value } }) => (
+                  <MUICheckbox
+                    id={id}
+                    disableRipple
+                    color="default"
+                    checkedIcon={
+                      <div className={`${checkboxSize} ${css[type]} ${css[`${type}--checked`]}`}>
+                        <Icon
+                          name="check"
+                          color={type === 'square' ? variables.color_primary_600 : 'white'}
+                          fontSize={fontSize}
+                        />
+                      </div>
+                    }
+                    icon={<div className={`${checkboxSize} ${css[type]} ${css[`${type}--checked`]}`} />}
+                    sx={{ padding: '0' }}
+                    checked={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
+            }
+            label={
+              label && (
+                <label htmlFor={id} className={`${css['label']} ml-2`} aria-describedby={id}>
+                  {label}
+                </label>
+              )
+            }
+          />
+        ) : (
+          <>
+            <MUICheckbox
+              id={id}
+              disableRipple
+              color="default"
+              checkedIcon={
+                <div className={`${checkboxSize} ${css[type]} ${css[`${type}--checked`]}`}>
+                  <Icon
+                    name="check"
+                    color={type === 'square' ? variables.color_primary_600 : 'white'}
+                    fontSize={fontSize}
+                  />
+                </div>
+              }
+              icon={<div className={`${checkboxSize} ${css[type]} ${css[`${type}--checked`]}`} />}
+              sx={{ padding: '0' }}
+              {...props}
+            />
+            {label && (
+              <label htmlFor={id} className={css.label} aria-describedby={id}>
+                {label}
+              </label>
+            )}
+          </>
         )}
       </div>
+      {errors &&
+        errors.map((e, index) => (
+          <p key={index} className={css['error']}>
+            {e}
+          </p>
+        ))}
     </div>
   );
 };
