37e0e4dfb98fab3c67584ac939586c121d09f961
satanmourner <mahmoudisanaz59@gmail.com>
2024-11-11 23:28:47 +0330

    refactor: remove bulk delete and checkboxes for selecting credentials on step-2

--- src/modules/Credential/containers/SchemaCredentialList/index.types.ts
+++ src/modules/Credential/containers/SchemaCredentialList/index.types.ts
@@ -1,8 +1,7 @@
-import { Credential, Schema } from 'src/core/adaptors';
+import { CredentialsRes, Schema } from 'src/core/adaptors';
 
 export interface SchemaCredentialListProps {
   selectedSchema: Schema;
-  selectedCredentials: string[];
-  onSelectCredential: (credentialId: string) => void;
-  onSelectAllCredentials: (checked: boolean, schemaCredentials: Credential[]) => void;
+  schemaCredentialList: CredentialsRes | null;
+  onUpdateSchemaCredentialList: (page: number) => void;
 }
--- src/modules/Credential/containers/SchemaCredentialList/useSchemaCredentialList.tsx
+++ src/modules/Credential/containers/SchemaCredentialList/useSchemaCredentialList.tsx
@@ -1,44 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CredentialsRes, CredentialStatus, deleteCredentialsAdaptor, getCredentialsAdaptor } from 'src/core/adaptors';
-import { StatusProps } from 'src/modules/General/components/Status/index.types';
+import { CredentialsRes, deleteCredentialAdaptor } from 'src/core/adaptors';
 
 export const useSchemaCredentialList = (
-  selectedSchemaId: string,
-  selectedCredentials: string[],
-  onSelectCredential: (credentialId: string) => void,
+  schemaCredentialList: CredentialsRes | null,
+  onUpdateSchemaCredentialList: (page: number) => void,
 ) => {
   const { t: translate } = useTranslation();
-  const [currentSchemaCredentialList, setCurrentSchemaCredentialList] = useState<CredentialsRes>();
-  const currentList = currentSchemaCredentialList?.items || [];
+  const currentList = schemaCredentialList?.items || [];
   const [openModal, setOpenModal] = useState<{
     name: 'add' | 'delete' | '';
     open: boolean;
+    credentialId?: string;
   }>({
     name: '',
     open: false,
+    credentialId: '',
   });
   const [page, setPage] = useState(1);
-  const totalPage = Math.ceil((currentSchemaCredentialList?.total || 1) / (currentSchemaCredentialList?.limit || 10));
-  const status: Record<CredentialStatus, StatusProps> = {
-    ISSUED: { label: translate('credential-status.issued'), theme: 'warning' },
-    PENDING: { label: translate('credential-status.pending'), theme: 'warning' },
-    ACTIVE: { label: translate('credential-status.active'), theme: 'success' },
-    REVOKED: { label: translate('credential-status.revoked'), theme: 'error' },
-    CREATED: { label: translate('credential-status.created'), theme: 'warning' },
-  };
-
-  useEffect(() => {
-    onChangePage(page);
-  }, [selectedSchemaId]);
+  const totalPage = Math.ceil((schemaCredentialList?.total || 1) / (schemaCredentialList?.limit || 10));
 
   const onChangePage = async (newPage: number) => {
     setPage(newPage);
-    const { data } = await getCredentialsAdaptor(newPage, 10, { schema_id: selectedSchemaId });
-    data && setCurrentSchemaCredentialList(data);
+    onUpdateSchemaCredentialList(newPage);
   };
 
-  const handleCloseModal = () => setOpenModal({ name: '', open: false });
+  const handleCloseModal = () => setOpenModal({ name: '', open: false, credentialId: '' });
 
   const onAddCredentialRecipientClick = () => setOpenModal({ name: 'add', open: true });
 
@@ -47,21 +34,21 @@ export const useSchemaCredentialList = (
     handleCloseModal();
   };
 
-  const onDeleteClick = () => setOpenModal({ name: 'delete', open: true });
+  const onDeleteClick = (credentialId: string) => setOpenModal({ name: 'delete', open: true, credentialId });
 
   const onDeleteCredential = async () => {
-    if (selectedCredentials) {
-      const { error } = await deleteCredentialsAdaptor(selectedCredentials);
+    const selectedCredential = openModal?.credentialId || '';
+    if (selectedCredential) {
+      const { error } = await deleteCredentialAdaptor(selectedCredential);
       if (error) return;
-      const filteredList = currentList.filter(list => !selectedCredentials.includes(list.id));
+      const filteredList = currentList.filter(list => list.id !== selectedCredential);
       onChangePage(filteredList.length === 0 && page > 1 ? page - 1 : page);
-      onSelectCredential('');
       handleCloseModal();
     }
   };
 
   return {
-    data: { translate, currentList, page, totalPage, status, openModal },
+    data: { translate, currentList, page, totalPage, openModal },
     operations: {
       onChangePage,
       onAddCredentialRecipientClick,
