6f020ca3d80f4369437a2840a2252ff8db40cdc1
satanmourner <mahmoudisanaz59@gmail.com>
2024-11-18 16:22:35 +0330

    refactor: fileUploaderMultiple to ProgressFileUploader

---
+++ src/modules/General/components/ProgressFileUploader/index.types.ts
@@ -0,0 +1,28 @@
+export type Files = {
+  id: string;
+  file: File;
+};
+
+export interface ProgressFileUploaderProps {
+  files: Files[];
+  fileTypes: string[];
+  onDropFiles: (files: File[]) => void;
+  onDeleteFiles: (fileId: string) => void;
+  showProgress?: boolean;
+  progressValues?: Record<string, number>;
+  uploadedErrors?: Record<string, boolean>;
+  maxSize?: number;
+  maxFiles?: number;
+  error?: string;
+  loading?: boolean;
+  multiple?: boolean;
+  customText?: string;
+  showSubtitle?: boolean;
+  customStyle?: string;
+}
+
+export interface fileInfo {
+  name: string;
+  type: string;
+  size: string;
+}
