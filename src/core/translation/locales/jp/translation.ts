bed0af142908a8d5542c1fb4a5c4ab5b95a8fb8c
satanmourner <mahmoudisanaz59@gmail.com>
2024-12-31 15:44:59 +0330

    feat: translations for file uploader

--- src/core/translation/locales/jp/translation.ts
+++ src/core/translation/locales/jp/translation.ts
@@ -1,10 +1,10 @@
 import credential from './credentials/credential.json';
 import credentialCreate from './credentials/credentialCreate.json';
 import credentialEmpty from './credentials/credentialEmpty.json';
-import fileUploader from './fileUploader.json';
 import forgetPassword from './forgetPassword.json';
 import layout from './general/layout.json';
 import pagination from './general/pagination.json';
+import uploader from './general/uploader.json';
 import integration from './integrations/integration.json';
 import kyb from './kyb/kyb.json';
 import orgProfile from './organizations/organizations.json';
@@ -23,14 +23,14 @@ import proofRequest from './verifications/proofRequest.json';
 export function generateTranslationFile() {
   return Object.assign(
     {},
-    pagination,
     layout,
+    pagination,
+    uploader,
     signIn,
     verification,
     detail,
     stepper,
     profile,
-    fileUploader,
     forgetPassword,
     schema,
     schemaCreate,
