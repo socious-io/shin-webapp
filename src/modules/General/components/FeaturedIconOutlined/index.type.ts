3132f3c926ad2726740a5f1167f7fbe01bdd1262
satanmourner <mahmoudisanaz59@gmail.com>
2025-01-02 15:59:33 +0330

    refactor: custom style prop for FeaturedIconOutlined

--- src/modules/General/components/FeaturedIconOutlined/index.type.ts
+++ src/modules/General/components/FeaturedIconOutlined/index.type.ts
@@ -2,4 +2,5 @@ export interface FeaturedIconOutlinedProps {
   size: 'sm' | 'md' | 'lg' | 'xl';
   theme: 'primary' | 'gray' | 'error' | 'warning' | 'success' | 'purple';
   iconName: string;
+  customStyle?: string;
 }
